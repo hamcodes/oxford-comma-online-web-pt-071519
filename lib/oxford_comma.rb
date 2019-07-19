de8130b0d7d9f1eac2e0d3f35d9614f12099c1b6
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-19 19:23:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,16 @@
+# my code passes first two examples
 # def oxford_comma(array)
 #   array.join(" and ")
 # end
-def oxford_comma(array)  
-  return array[0] if array.length == 1
-  return array[0..-2].join(', ') + " and " + array[-1] if array.length > 1
+
+#code found on stack overflow passes first two examples
+# def oxford_comma(array)  
+#   return array[0] if array.length == 1
+#   return array[0..-2].join(', ') + " and " + array[-1] if array.length > 1
  
+# end
+
+def oxford_comma(array)
+  return array.to_s if array.nil? or array.length <= 1
+  array[0..-2].join(", ") + " and " + array[-1]
 end
