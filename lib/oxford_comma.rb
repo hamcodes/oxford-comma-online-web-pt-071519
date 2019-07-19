3b4a041aa672eddfec841a27fed03539f1f37ae0
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-19 19:32:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,12 +3,7 @@
 #   array.join(" and ")
 # end
 
-#code found on stack overflow passes first two examples
-# def oxford_comma(array)  
-#   return array[0] if array.length == 1
-#   return array[0..-2].join(', ') + " and " + array[-1] if array.length > 1
- 
-# end
+
 def oxford_comma(array)
   if array.length == 1
     return "#{array[0]}"
