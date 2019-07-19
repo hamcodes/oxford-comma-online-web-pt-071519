0965de1172747cbe500876a180f57e938680dc3e
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-19 19:19:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,5 +4,6 @@
 def oxford_comma(array)  
   return nil if array.nil?
   return array[0] if array.length == 1
-  return array[0..-2].join(',') + " and " + array[-1] if array.length > 1
+  return array[0..-8].join(', ') + " and " + array[-1] if array.length > 1
+ 
 end
