fbdae0644faa3c783b40a69cfa08efd9385c6a68
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-19 19:20:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@
 #   array.join(" and ")
 # end
 def oxford_comma(array)  
-  return nil if array.nil?
+  
   return array[0] if array.length == 1
   return array[0..-2].join(', ') + " and " + array[-1] if array.length > 1
  
